2dd92126862c469d832be038e416374c4d5b4e66
fqiang <fqiang@gmail.com>
2016-06-06 14:57:13 -0500

    adding scenarios generation in parallel

--- src/StructJuMP.jl
+++ src/StructJuMP.jl
@@ -15,11 +15,11 @@ export StructuredModel, getStructure, getparent, getchildren, getProcIdxSet,
 
 type StructureData
     probability::Vector{Float64}
-    children::Vector{JuMP.Model}
+    children::Dict{Int,JuMP.Model}
     parent
     num_scen::Int
-    # othervars::Vector{JuMP.Variable}
     othermap::Dict{JuMP.Variable,JuMP.Variable}
+    comm::MPI.Comm
 end
 
 default_probability(m::JuMP.Model) = 1 / num_scenarios(m)
@@ -30,12 +30,19 @@ default_probability(::Void) = 1.0
 # ---------------
 
 # Constructor with the number of scenarios
-function StructuredModel(;solver=JuMP.UnsetSolver(), parent=nothing, same_children_as=nothing, num_scenarios::Int=0, prob::Float64=default_probability(parent))
+function StructuredModel(;solver=JuMP.UnsetSolver(), parent=nothing, same_children_as=nothing, id=0, num_scenarios::Int=0, prob::Float64=default_probability(parent))
     m = JuMP.Model(solver=solver)
     if parent !== nothing
+        @assert id != 0 
         stoch = getStructure(parent)
-        push!(stoch.children, m)
+        stoch.children[id] = m
         push!(stoch.probability, prob)
+        comm = stoch.comm
+        @assert comm == MPI.COMM_WORLD
+    else
+        id = 0
+        MPI.Init()
+        comm = MPI.COMM_WORLD
     end
     if same_children_as !== nothing
       if !isa(same_children_as, JuMP.Model) || !haskey(same_children_as.ext, :Stochastic)
@@ -45,9 +52,9 @@ function StructuredModel(;solver=JuMP.UnsetSolver(), parent=nothing, same_childr
       children = same_children_as.ext[:Stochastic].children
     else
       probability = Float64[]
-      children = JuMP.Model[]
+      children = Dict{Int, JuMP.Model}()
     end
-    m.ext[:Stochastic] = StructureData(Float64[], JuMP.Model[], parent, num_scenarios, Dict{JuMP.Variable,JuMP.Variable}())
+    m.ext[:Stochastic] = StructureData(probability, children, parent, num_scenarios, Dict{JuMP.Variable,JuMP.Variable}(), comm)
     m
 end
 
